9054ebb10a4846a568d696c2c27b8685117bfb24
Yi-Jiahe <47210363+Yi-Jiahe@users.noreply.github.com>
2021-11-06 09:35:08 +0800

    abstract results drawing and image show into a function
    Former-commit-id: 0701018d4957568322e6299fe492f1143f9a3ec4

--- main.py
+++ main.py
@@ -2,63 +2,69 @@
 from cv2 import cv2
 import mediapipe as mp
 
-# INITIALIZING OBJECTS
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_face_mesh = mp.solutions.face_mesh
-mp_hands = mp.solutions.hands
 
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-cap = cv2.VideoCapture(0)
+def debug(image, mp_face_mesh, results_face, mp_hands, results_hands):
+    # To improve performance
+    image.flags.writeable = True
 
-# DETECT THE FACE LANDMARKS
-with mp_face_mesh.FaceMesh(refine_landmarks=True, min_detection_confidence=0.8, min_tracking_confidence=0.8) as face_mesh, \
-        mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) as hands:
-    while True:
-        success, image = cap.read()
+    # Convert back to the BGR color space
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        # Flip the image horizontally and convert the color space from BGR to RGB
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # Draw the face mesh annotations on the image.
+    if results_face.multi_face_landmarks:
+        for face_landmarks in results_face.multi_face_landmarks:
+            mp_drawing.draw_landmarks(
+                image=image,
+                landmark_list=face_landmarks,
+                connections=mp_face_mesh.FACEMESH_TESSELATION,
+                landmark_drawing_spec=None,
+                connection_drawing_spec=mp_drawing_styles
+                    .get_default_face_mesh_tesselation_style())
 
-        # To improve performance
-        image.flags.writeable = False
+    if results_hands.multi_hand_landmarks:
+        for hand_landmarks in results_hands.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
 
-        # Detect the face landmarks
-        results_face = face_mesh.process(image)
-        results_hands = hands.process(image)
+    # Display the image
+    cv2.imshow('MediaPipe FaceMesh and Hands', image)
 
-        # To improve performance
-        image.flags.writeable = True
 
-        # Convert back to the BGR color space
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+if __name__ == '__main__':
+    # INITIALIZING OBJECTS
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+    mp_face_mesh = mp.solutions.face_mesh
+    mp_hands = mp.solutions.hands
 
-        # Draw the face mesh annotations on the image.
-        if results_face.multi_face_landmarks:
-            for face_landmarks in results_face.multi_face_landmarks:
-                mp_drawing.draw_landmarks(
-                    image=image,
-                    landmark_list=face_landmarks,
-                    connections=mp_face_mesh.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles
-                        .get_default_face_mesh_tesselation_style())
+    drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+    cap = cv2.VideoCapture(0)
 
-        if results_hands.multi_hand_landmarks:
-            for hand_landmarks in results_hands.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    image,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style())
+    # DETECT THE FACE LANDMARKS
+    with mp_face_mesh.FaceMesh(refine_landmarks=True, min_detection_confidence=0.8, min_tracking_confidence=0.8) as face_mesh, \
+            mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.8) as hands:
+        while True:
+            success, image = cap.read()
 
-        # Display the image
-        cv2.imshow('MediaPipe FaceMesh and Hands', image)
+            # Flip the image horizontally and convert the color space from BGR to RGB
+            image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
 
-        # Terminate the process
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
+            # To improve performance
+            image.flags.writeable = False
 
-cap.release()
-cv2.destroyAllWindows()
+            # Detect the face landmarks
+            results_face = face_mesh.process(image)
+            results_hands = hands.process(image)
+
+            debug(image, mp_face_mesh, results_face, mp_hands, results_hands)
+
+            # Terminate the process
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
